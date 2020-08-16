63c66cb7363554274323b65a5fdfc39ac612a106
vasjaj <vjelcaniniovs@gmail.com>
2020-08-16 16:37:40 +0300

    refactor to non js frontend

--- client/go.mod
+++ client/go.mod
@@ -1,3 +1,5 @@
 module github.com/vasjaj/Scoreboard/client
 
 go 1.14
+
+require github.com/spf13/cobra v1.0.0
---
+++ client/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/vasjaj/Scoreboard/client/cmd/cmd"
+
+func main() {
+	cmd.Execute()
+}
